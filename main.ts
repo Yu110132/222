187afa49b861d634a4484efb94a578fe6d39dbf0
Yu110132 <131228123+Yu110132@users.noreply.github.com>
2023-05-03 11:59:47 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,58 @@
+let dd = 0
+input.onButtonPressed(Button.A, function () {
+    dd = randint(0, 6)
+    if (dd == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (dd == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (dd == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    }
+    if (dd == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (dd == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (dd == 6) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
